45e330fc9ff8721fe1c9db64dd17eb7ef8839ce0
vaibhav-xp <vibhu.astute@gmail.com>
2026-01-06 23:43:35 +0530

    fix(footer): updated footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,34 @@
-import { SOURCE_CODE_GITHUB_URL } from "@/config/site-config";
+import { SOURCE_CODE_GITHUB_URL, UTM_PARAMS } from "@/config/site-config";
+import { addQueryParams } from "@/lib/url";
 
 export function Footer() {
   return (
     <footer className="overflow-x-hidden px-2">
       <div className="screen-line-after mx-auto border-x border-edge py-4 md:max-w-3xl">
         <p className="mb-1 px-4 text-center font-mono text-sm text-balance text-muted-foreground">
-          Inspired by tailwindcss.com & ui.shadcn.com
+          Design inspired by{" "}
+          <a
+            className="link"
+            href={addQueryParams("https://chanhdai.com/", UTM_PARAMS)}
+            target="_blank"
+            rel="noopener"
+          >
+            Chanhdai
+          </a>
+          . Lightweight adaptation
         </p>
 
         <p className="px-4 text-center font-mono text-sm text-balance text-muted-foreground">
           Built by{" "}
           <a
             className="link"
-            href="https://x.com/iamncdai"
+            href="https://x.com/hevaibhavmaurya"
             target="_blank"
             rel="noopener"
           >
-            ncdai
+            Vaibhav
           </a>
-          . The source code is available on{" "}
+          . Source code available on{" "}
           <a
             className="link"
             href={SOURCE_CODE_GITHUB_URL}
